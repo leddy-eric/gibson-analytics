68affef441736d08b293e47565ac59569fd23773
leddy.eric <eric.leddy@level3.com>
2019-06-14 12:49:41 -0600

    Fix estimated lineups

--- gibson-ui-web/src/main/java/com/gibson/analytics/core/baseball/repository/MlbGameDetailRepository.java
+++ gibson-ui-web/src/main/java/com/gibson/analytics/core/baseball/repository/MlbGameDetailRepository.java
@@ -5,7 +5,10 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import com.gibson.analytics.core.baseball.data.MlbGameDetail;
+import com.gibson.analytics.core.baseball.data.MlbGameStatus;
 import com.gibson.analytics.enums.MlbTeamLookup;
 
 /**
@@ -31,19 +34,12 @@ public interface MlbGameDetailRepository extends JpaRepository<MlbGameDetail, Lo
 	 */
 	public Optional<MlbGameDetail> findByApiId(String apiId);
 	
-	/**
-	 * Find the last game for the given home team.
-	 * 
-	 * @param team
-	 * @return
-	 */
-	public Optional<MlbGameDetail> findTopByHomeTeamOrderByCreated(MlbTeamLookup team);
 	
 	/**
-	 * Find the last game for the given home team.
 	 * 
 	 * @param team
 	 * @return
 	 */
-	public Optional<MlbGameDetail> findTopByAwayTeamOrderByCreated(MlbTeamLookup team);
+	@Query("SELECT g FROM MlbGameDetail g where g.home.team = ?1 or g.away.team = ?1 and g.status in ('FINAL','RECOMMEND') ORDER BY created DESC")
+	public List<MlbGameDetail> findLatestByTeam(MlbTeamLookup team);
 }
